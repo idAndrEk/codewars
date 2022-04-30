496fd0ca60393f8ed081619bcb02aa6f96b7625e
Andrey <AndrEkat96@yandex.ru>
2022-05-01 01:30:10 +0500

    tasks CW

--- tasks_13.js
+++ tasks_13.js
@@ -1,7 +1,7 @@
 const rps = (p1, p2) => {
     if (p1 == p2) {
         return 'Draw!';
-    } if (p1 == 'rock' && p2 == 'scissors') {
+    } else if (p1 == 'rock' && p2 == 'scissors') {
         return 'Player 1 won!'
     } else if (p1 == 'scissors' && p2 == 'paper') {
         return 'Player 1 won!'
@@ -10,4 +10,3 @@ const rps = (p1, p2) => {
     } else
         return 'Player 2 won!';
 }
-   }
